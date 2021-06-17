afe2723b6b4adc03e412cc69156ab81e38545d93
Anna Helfferich <anna.helfferich@gmx.net>
2021-06-17 13:08:06 +0200

    starting with breakpoints/ small_nav

--- 03_js/nav_dropdown.js
+++ 03_js/nav_dropdown.js
@@ -18,3 +18,15 @@ $(".nav_left-dropdown-btn").hover(
     }, 6000);
   }
 );
+
+/* SMALL */
+const sem_button = document.querySelector(".nav_left-dropdown-btn");
+const small_nav = document.querySelector(".small_nav");
+const closeSmall = document.querySelector(".click");
+
+sem_button.addEventListener("click", (e) => {
+  small_nav.style.marginLeft = "2rem";
+});
+closeSmall.addEventListener("click", (e) => {
+  small_nav.style.marginLeft = "";
+});
